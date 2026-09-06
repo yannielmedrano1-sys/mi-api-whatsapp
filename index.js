1beca624802ca8d98dba617a2351d4f91c7a3fb3
yannielmedrano1-sys <yannielmedrano@gmail.com>
2026-09-06 01:24:10 -0400

    Migrando a distube ytdl-core para arreglar descargas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import yts from 'yt-search';
-import ytdl from 'ytdl-core';
+import ytdl from '@distube/ytdl-core';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
